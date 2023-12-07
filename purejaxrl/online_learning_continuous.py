90b915c825282b65dd9ace754b1b41d4ee5ee9da
Jacob Adkins <jadkins@ualberta.ca>
2023-12-07 15:34:56 -0700

    log_std is learned instead of constant

--- purejaxrl/online_learning_continuous.py
+++ purejaxrl/online_learning_continuous.py
@@ -55,18 +55,21 @@ class Actor(nn.Module):
             activation = nn.relu
         else:
             activation = nn.tanh
-        actor_mean = nn.Dense(
+        actor_rep = nn.Dense(
             256, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
         )(x)
-        actor_mean = activation(actor_mean)
-        actor_mean = nn.Dense(
+        actor_rep = activation(actor_rep)
+        actor_rep = nn.Dense(
             256, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
-        )(actor_mean)
-        actor_mean = activation(actor_mean)
+        )(actor_rep)
+        actor_rep = activation(actor_rep)
         actor_mean = nn.Dense(
             self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
-        )(actor_mean)
-        actor_logtstd = self.param("log_std", nn.initializers.zeros, (self.action_dim,))
+        )(actor_rep)
+        actor_logtstd = nn.Dense(
+            self.action_dim, kernel_init=orthogonal(0.0), bias_init=constant(0.0)
+        )(actor_rep)
+        # actor_logtstd = self.param("log_std", nn.initializers.zeros, (self.action_dim,))
         pi = distrax.MultivariateNormalDiag(actor_mean, jnp.exp(actor_logtstd))
 
         return pi
